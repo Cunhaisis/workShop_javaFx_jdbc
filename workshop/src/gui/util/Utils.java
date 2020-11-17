1369c1eccf3cae7f13e079b7712dbebae8445041
Cunhaisis <isis.cunha13@gmail.com>
2020-11-17 20:22:59 -0300

    Saving a new Department

--- workshop/src/gui/util/Utils.java
+++ workshop/src/gui/util/Utils.java
@@ -1,14 +1,23 @@
 package gui.util;
 
 import javafx.event.ActionEvent;
-import javafx.stage.Stage;
 import javafx.scene.Node;
+import javafx.stage.Stage;
 
 
 public class Utils {
 	
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+		return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
 	}
 
 }
